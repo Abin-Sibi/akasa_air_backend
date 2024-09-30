bcce5a57c8957ed8ac4e833ab305b5f65aae8c8e
AbinSibi466 <abin.sibi@mca.christuniversity.in>
2024-09-30 15:12:19 +0530

    changed to atlas

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 const connect= ()=>{
-    mongoose.connect('mongodb://localhost:27017/akasa',{
-
+    mongoose.connect(process.env.MONGODB_URI,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
     }).then(()=>{console.log('DB connected')
     }).catch((err)=>{console.log(err)})
 }
